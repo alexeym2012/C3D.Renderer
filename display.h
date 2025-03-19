32d30e844fe6a7c88f082df0ababcb9ba90b7e7b
Alexey Murashkevich <alexey@ichannel.io>
2025-03-19 17:43:21 +0200

    Added Mesh, Triangle, Faces rendering and line drawing using DDA algorythm

--- display.h
+++ display.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <SDL.h>
 
-#define FPS 200 
+#define FPS 60 
 #define FRAME_TARGET_TIME (1000 / FPS)
 
 extern SDL_Window* window;
@@ -18,6 +18,8 @@ extern int window_height;
 bool initialize_window(void); 
 void draw_grid(void);
 void draw_pixel(int x, int y, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
 void render_color_buffer(void); 
 void clear_color_buffer(uint32_t color);
--- main.c
+++ main.c
@@ -4,15 +4,12 @@
 #include <SDL.h>
 #include "display.h"
 #include "vector.h"
+#include "mesh.h"
 
-////////////////////////////////////////////////////////////////////////////////
-// Declare an array of vectors/points
-////////////////////////////////////////////////////////////////////////////////
-#define N_POINTS (9 * 9 * 9)
-vec3_t cube_points[N_POINTS]; // 9x9x9 cube
-vec2_t projected_points[N_POINTS];
+triangle_t triangles_to_render[N_MESH_FACES];
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+float camera_increment = -0.1;
 vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
 
 float fov_factor = 640;
@@ -21,125 +18,146 @@ bool is_running = false;
 int previous_frame_time = 0;
 
 void setup(void) {
-    // Allocate the required memory in bytes to hold the color buffer
-    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+	// Allocate the required memory in bytes to hold the color buffer
+	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
 
-    // Creating a SDL texture that is used to display the color buffer
-    color_buffer_texture = SDL_CreateTexture(
-        renderer,
-        SDL_PIXELFORMAT_ARGB8888,
-        SDL_TEXTUREACCESS_STREAMING,
-        window_width,
-        window_height
-    );
-
-    int point_count = 0;
-
-    // Start loading my array of vectors
-    // From -1 to 1 (in this 9x9x9 cube)
-    for (float x = -1; x <= 1; x += 0.25) {
-        for (float y = -1; y <= 1; y += 0.25) {
-            for (float z = -1; z <= 1; z += 0.25) {
-                vec3_t new_point = { .x = x, .y = y, .z = z };
-                cube_points[point_count++] = new_point;
-            }
-        }
-    }
+	// Creating a SDL texture that is used to display the color buffer
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
 }
 
 void process_input(void) {
-    SDL_Event event;
-    SDL_PollEvent(&event);
+	SDL_Event event;
+	SDL_PollEvent(&event);
 
-    switch (event.type) {
-        case SDL_QUIT:
-            is_running = false;
-            break;
-        case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE)
-                is_running = false;
-            break;
-    }
+	switch (event.type) {
+	case SDL_QUIT:
+		is_running = false;
+		break;
+	case SDL_KEYDOWN:
+		if (event.key.keysym.sym == SDLK_ESCAPE)
+			is_running = false;
+		break;
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Function that receives a 3D vector and returns a projected 2D point
 ////////////////////////////////////////////////////////////////////////////////
 vec2_t project(vec3_t point) {
-    vec2_t projected_point = {
-        .x = (fov_factor * point.x) / point.z,
-        .y = (fov_factor * point.y) / point.z
-    };
-    return projected_point;
+	vec2_t projected_point = {
+		.x = (fov_factor * point.x) / point.z,
+		.y = (fov_factor * point.y) / point.z
+	};
+	return projected_point;
 }
 
 void update(void) {
-    // Wait some time until the reach the target frame time in milliseconds
-    int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+	// Wait some time until the reach the target frame time in milliseconds
+	int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
 
-    // Only delay execution if we are running too fast
-    if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
-        SDL_Delay(time_to_wait);
-    }
+	// Only delay execution if we are running too fast
+	if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+		SDL_Delay(time_to_wait);
+	}
 
-    previous_frame_time = SDL_GetTicks();
+	previous_frame_time = SDL_GetTicks();
 
-    cube_rotation.x += 0.01;
-    cube_rotation.y += 0.01;
-    cube_rotation.z += 0.01;
+	cube_rotation.x += 0.01;
+	cube_rotation.y += 0.01;
+	cube_rotation.z += 0.01;
 
-    for (int i = 0; i < N_POINTS; i++) {
-        vec3_t point = cube_points[i];
+	camera_position.z += camera_increment;
 
-        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
-        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
-        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+	if (camera_position.z > -3 || camera_position.z < -15) {
+		camera_increment *= -1;
+	}
 
-        // Translate the points away from the camera
-        transformed_point.z -= camera_position.z;
+	// Loop all triangle faces of our mesh
+	for (int i = 0; i < N_MESH_FACES; i++) {
+		face_t mesh_face = mesh_faces[i];
 
-        // Project the current point
-        vec2_t projected_point = project(transformed_point);
+		vec3_t face_vertices[3];
+		face_vertices[0] = mesh_vertices[mesh_face.a - 1];
+		face_vertices[1] = mesh_vertices[mesh_face.b - 1];
+		face_vertices[2] = mesh_vertices[mesh_face.c - 1];
 
-        // Save the projected 2D vector in the array of projected points
-        projected_points[i] = projected_point;
-    }
+		triangle_t projected_triangle;
+
+		// Loop all three vertices of this current face and apply transformations
+		for (int j = 0; j < 3; j++) {
+			vec3_t transformed_vertex = face_vertices[j];
+
+			transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
+			transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
+			transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+
+			// Translate the vertex away from the camera
+			transformed_vertex.z -= camera_position.z;
+
+			// Project the current vertex
+			vec2_t projected_point = project(transformed_vertex);
+
+			// Scale and translate the projected points to the middle of the screen
+			projected_point.x += (window_width / 2);
+			projected_point.y += (window_height / 2);
+
+			projected_triangle.points[j] = projected_point;
+		}
+
+		// Save the projected triangle in the array of triangles to render
+		triangles_to_render[i] = projected_triangle;
+	}
 }
 
 void render(void) {
-    draw_grid();
+	draw_grid();
 
-    // Loop all projected points and render them
-    for (int i = 0; i < N_POINTS; i++) {
-        vec2_t projected_point = projected_points[i];
-        draw_rect(
-            projected_point.x + (window_width / 2),
-            projected_point.y + (window_height / 2),
-            4,
-            4,
-            0xFFFFFF00
-        );
-    }
+	// Loop all projected triangles and render them
+	for (int i = 0; i < N_MESH_FACES; i++) {
+		triangle_t triangle = triangles_to_render[i];
 
-    render_color_buffer();
+		// Draw vertex points
+		draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
+		draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFAA0000);
+		draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFCCAA00);
 
-    clear_color_buffer(0xFF000000);
+		// Draw unfilled triangle
+		draw_triangle(
+			triangle.points[0].x,
+			triangle.points[0].y,
+			triangle.points[1].x,
+			triangle.points[1].y,
+			triangle.points[2].x,
+			triangle.points[2].y,
+			0xFF00FF00
+		);
+	}
 
-    SDL_RenderPresent(renderer);
+	render_color_buffer();
+
+	clear_color_buffer(0xFF000000);
+
+	SDL_RenderPresent(renderer);
 }
 
 int main(void) {
-    is_running = initialize_window();
+	is_running = initialize_window();
 
-    setup();
+	setup();
 
-    while (is_running) {
-        process_input();
-        update();
-        render();
-    }
+	while (is_running) {
+		process_input();
+		update();
+		render();
+	}
 
-    destroy_window();
+	destroy_window();
 
-    return 0;
+	return 0;
 }
